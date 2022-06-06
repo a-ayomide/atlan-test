4ab6d894b45f355c5c6f78c7d7e3ddab84f56a29
Daniel Apantaku <apantakuayo@gmail.com>
2022-06-06 23:24:10 +0100

    init commit

---
+++ atlan-test/src/App.jsx
@@ -0,0 +1,13 @@
+import { Button, ButtonGroup } from '@chakra-ui/react'
+function App() {
+
+  return (
+    <div className="App">
+      <h1>
+        Testing
+      </h1>
+    </div>
+  )
+}
+
+export default App
